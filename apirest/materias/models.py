466f712c8e866a1a9ca36e1679fdf75d2231924e
CarlosW <carlos-walte@hotmail.com>
2018-05-25 14:15:18 -0300

    Iniciando a api, adicionando a classe materias

---
+++ apirest/materias/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class materias(models.Model) :
+    nome = models.CharField(max_length=200)
+    ab1 = models.DecimalField(max_digits=6, decimal_places=2)
+    ab2 = models.DecimalField(max_digits=6, decimal_places=2)
+    reav = models.DecimalField(max_digits=6, decimal_places=2)
+    final = models.DecimalField(max_digits=6, decimal_places=2)
+    media = models.DecimalField(max_digits=6, decimal_places=2)
+    faltas = models.IntegerField()    
+    carga_horaria = models.IntegerField()
+    max_faltas = models.IntegerField()
+    conceito = models.CharField(max_length=200)
+
+
