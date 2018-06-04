84c7cc16749cd75dd48e0c658f18d4dad8ab632d
CarlosW1998 <carlos-walte@hotmail.com>
2018-06-04 19:59:59 -0300

    Finalizando as mudanças no CRUD, e comentando os endpoints

--- apirest/materias/views.py
+++ apirest/materias/views.py
@@ -44,12 +44,23 @@ class detalhesDasMaterias(generics.RetrieveUpdateDestroyAPIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = materiaSerializer(materia, many = True)
         return Response(serializer.data)
+    
+    def update(self, request, pk) :
+        try :
+            materia = materias.objects.get(pk = pk, usuario = request.user)
+        except materias.DoesNotExist :
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = materiaSerializer(materia, data = request.data)
+        if serializer.is_valid() :
+            serializer.save(usuario = request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk) :
         try :
-            materia = materias.objects.filter(pk = pk, user = request.user)
+            materia = materias.objects.filter(pk = pk, usuario = request.user)
         except materias.DoesNotExist :
-            return Response(status=status.HTTP_404_NOT_FOUND) 
+            return Response(status=status.HTTP_204_NO_CONTENT) 
         materia.delete()
         return Response(status = status.HTTP_205_RESET_CONTENT)
 
