264655ca7c05b0764c956a5d9f053865f83a2fdf
CarlosW <carlos-walte@hotmail.com>
2018-06-06 09:31:49 -0300

    resolvendo conflitos no merge

--- apirest/materias/urls.py
+++ apirest/materias/urls.py
@@ -5,13 +5,13 @@ from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 urlpatterns = [
     #Recebe o usuario e a senha, num metodo post e retorna um token para usar a api
-    #Fora a criação de usuario todos os metodos precisam estar autenticados para poderem ser acessados
+    #Fora a criacao de usuario todos os metodos precisam estar autenticados para poderem ser acessados
     url(r'^api-token-auth/', obtain_jwt_token),
 
     #Recebe um usuario e senha, num metodo POST e cria um novo usuario
     url(r'^criarusuario/$', views.criarUsuario.as_view()),
 
-    #Recebendo um metodo GET retorna todas a materias, 
+    #Recebendo um metodo GET retorna todas a materias,
     #Recebendo um metodo POST cia uma nova materia, precisa dos dados pra nova materia
     url(r'^materias/$', views.listaDeMaterias.as_view()),
 
